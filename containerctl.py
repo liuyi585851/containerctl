84012edda7c7bd2b584abfe4e0515740895629c3
liuyi585851 <liuyi585851@outlook.com>
2024-02-16 21:45:20 +0800

    Add

--- containerctl.py
+++ containerctl.py
@@ -3,6 +3,10 @@ import sys
 import getopt
 import os
 import json
+import requests
+import tarfile
+from tqdm import tqdm
+
 # 检查软件包是否安装
 def check_package(package_name):
     try:
@@ -25,6 +29,75 @@ def install_package(package_name):
             print(f"软件包 {package_name} 安装成功")
     except Exception as e:
         print(f"发生未知错误，错误原因: {e}")
+
+# 文件解压
+def untar(file_path, target_path):
+    tar = tarfile.open(file_path)
+    members = tar.getmembers()
+    progress_bar = tqdm(total=len(members), desc="解压中")
+    for member in members:
+        tar.extract(member, path=target_path)
+        progress_bar.update(1)
+    progress_bar.close()
+    tar.close()
+
+# 文件下载
+def download_file(url, headers,path):
+    res = requests.get(url, headers=headers,stream=True)
+    chunk_size = 1024 * 4
+    content_size = int(res.headers['content-length'])
+    data_count = 0
+    with open(path, "wb") as file:
+        progress = tqdm(total=content_size, unit="B", unit_scale=True, desc="下载中")
+        for data in res.iter_content(chunk_size=chunk_size):
+                progress.update(len(data))
+                file.write(data)
+                
+# 获取token
+def get_token(image):  
+    url = f"https://auth.docker.io/token?service=registry.docker.io&scope=repository:library/{image}:pull"
+    res = requests.get(url)
+    data = json.loads(res.text)
+    return data['access_token']
+    
+# 获取digest
+def get_digest(image,tag):
+    url = f"https://registry-1.docker.io/v2/library/{image}/manifests/{tag}"
+    headers = {
+        'Authorization': f"Bearer {get_token(image)}"
+    }
+    res = requests.get(url,headers=headers)
+    data = json.loads(res.text)
+    data = data['manifests'][0]
+    return data['digest']
+
+# 获取manifest
+def get_manifest(image,tag):
+    url = f"https://registry-1.docker.io/v2/library/{image}/manifests/{get_digest(image,tag)}"
+    headers = {
+        'Authorization': f"Bearer {get_token(image)}",
+        'Accept': 'application/vnd.oci.image.manifest.v1+json'
+    }
+    res = requests.get(url,headers=headers)
+    return res.text
+
+# 拉取镜像
+def pull_image(image,tag,name):
+    manifest = get_manifest(image,tag)
+    manifest = json.loads(manifest)
+    layers = manifest['layers']
+    count=0
+    for layer in layers:
+        count+=1
+        print(f"正在拉取镜像层{count}...")
+        url = f"https://registry-1.docker.io/v2/library/{image}/blobs/{layer['digest']}"
+        headers = {   
+            'Authorization': f"Bearer {get_token(image)}",
+            'Accept': 'application/vnd.oci.image.layer.v1.tar+gzip'
+        }
+        os.makedirs(f"containers/{name}/images/blobs",exist_ok=True)
+        download_file(url,headers,f"containers/{name}/images/blobs/layer-{count}.tar.gz")
+        untar(f"containers/{name}/images/blobs/layer-{count}.tar.gz",f"containers/{name}/images/rootfs")
 
 def rollback(name):
     try:
@@ -62,42 +135,19 @@ def operation_create():
         sys.exit()
     try:
         print(f"正在拉取镜像 {image}:{tag} ...")
-        result=subprocess.run(['skopeo','copy',f'docker://docker.io/{image}:{tag}',f'oci:containers/{name}/images'], capture_output=True,text=True)
-        if result.returncode!=0:
+        try:
+            pull_image(image,tag,name)
+            print(f"拉取镜像 {image}:{tag} 成功")
+        except:
             print(f"拉取镜像 {image}:{tag} 失败，请检查网络连接或镜像是否存在")
             rollback(name)
             sys.exit()
-        else:
-            print(f"镜像 {image}:{tag} 拉取成功")
     except Exception as e:
         print(f"发生未知错误，错误原因:{e}")
         rollback(name)
         sys.exit()
     try:
-        print(f"正在创建镜像 {image}:{tag} bundle...")
-        result=subprocess.run(['oci-image-tool','create','--ref','platform.os=linux',f'containers/{name}/images',f'containers/{name}/images-bundle'], capture_output=True,text=True)
-        if result.returncode!=0:
-            print(f"创建镜像 {image}:{tag} bundle失败，请检查是否已存在")
-            rollback(name)
-            sys.exit()
-        else:
-            print(f"镜像 {image}:{tag} bundle创建成功")
-    except Exception as e:
-        print(f"发生未知错误，错误原因:{e}")
-        rollback(name)
-        sys.exit()
-    try:
-        result=subprocess.run(['rm','-rf',f'containers/{name}/images-bundle/config.json'], capture_output=True,text=True)
-        if result.returncode!=0:
-            print(f"发生未知错误")
-            rollback(name)
-            sys.exit()
-    except Exception as e:
-        print(f"发生未知错误，错误原因:{e}")
-        rollback(name)
-        sys.exit()
-    try:
-        result=subprocess.run(['runc','spec','-b',f'containers/{name}/images-bundle'], capture_output=True,text=True)
+        result=subprocess.run(['runc','spec','-b',f'containers/{name}/images'], capture_output=True,text=True)
         if result.returncode!=0:
             print(f"生成runc配置文件失败")
             rollback(name)
@@ -109,19 +159,19 @@ def operation_create():
         rollback(name)
         sys.exit()
     try:
-        with open(f'containers/{name}/images-bundle/config.json','r') as f:
+        with open(f'containers/{name}/images/config.json','r') as f:
             config=json.load(f)
         
         config['process']['terminal']=False
         config['process']['args']=['sleep','1000000']
-        with open(f'containers/{name}/images-bundle/config.json','w') as f:
+        with open(f'containers/{name}/images/config.json','w') as f:
             json.dump(config,f)
     except:
         print(f"发生未知错误")
         rollback(name)
         sys.exit()
     try:
-        result=subprocess.run(['runc','create',f'--bundle=containers/{name}/images-bundle',f'{name}'])
+        result=subprocess.run(['runc','create',f'--bundle=containers/{name}/images',f'{name}'])
         if result.returncode!=0:
             print(f"创建容器 {name} 失败")
             rollback(name)
@@ -169,7 +219,7 @@ def operation_start():
         sys.exit()
     try:
         result1=subprocess.run(['runc','delete',f'{name}'],capture_output=True,text=True)
-        result2=subprocess.run(['runc','create',f'--bundle=containers/{name}/images-bundle',f'{name}'])
+        result2=subprocess.run(['runc','create',f'--bundle=containers/{name}/images',f'{name}'])
         result3=subprocess.run(['runc','start',f'{name}'],capture_output=True,text=True)
         if result1.returncode!=0 or result2.returncode!=0 or result3.returncode!=0:
             print(f"容器 {name} 启动失败")
@@ -263,8 +313,6 @@ if os.geteuid()!=0:
     sys.exit()
 # 运行依赖检查
 check_package("runc")
-check_package("skopeo")
-check_package("oci-image-tool")
 cmd = sys.argv
 cnt=0
 for values in cmd:
